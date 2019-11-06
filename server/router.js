5f230c808e76ef86e567a93e9c2b53c9f9f86cbc
‘Peekz1025’ <peekz1025@yahoo.com>
2019-11-05 21:40:40 -0500

    done probably

--- server/router.js
+++ server/router.js
@@ -2,9 +2,11 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
+  app.get('/getToken', mid.requireSecure, controllers.Account.getToken);
+  app.get('/getDomos', mid.requireLogin, controllers.Domo.getDomos);
+
   app.get('/login', mid.requireSecure, mid.requireLogout, controllers.Account.loginPage);
   app.post('/login', mid.requireSecure, mid.requireLogout, controllers.Account.login);
-  app.get('/signup', mid.requireSecure, mid.requireLogout, controllers.Account.signupPage);
   app.post('/signup', mid.requireSecure, mid.requireLogout, controllers.Account.signup);
   app.get('/logout', mid.requireLogin, controllers.Account.logout);
   app.get('/maker', mid.requireLogin, controllers.Domo.makerPage);
